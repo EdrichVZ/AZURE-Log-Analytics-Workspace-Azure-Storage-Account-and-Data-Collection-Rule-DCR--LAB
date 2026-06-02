0e359dc79a6d74d4a6375917291c9136670b9748
Edrich van Zyl <171524023+EdrichVZ@users.noreply.github.com>
2026-06-02 08:54:33 +0200

    Add Azure resource group and VM creation script

--- Screenshots/scripts/task1.ps1
+++ Screenshots/scripts/task1.ps1
@@ -1,1 +1,5 @@
+ New-AzResourceGroup -Name AZ500LAB131415 -Location 'EastUS'
 
+ Register-AzProviderFeature -FeatureName "EncryptionAtHost" -ProviderNamespace Microsoft.Compute 
+
+  New-AzVm -ResourceGroupName "AZ500LAB131415" -Name "myVM" -Location 'EastUS' -VirtualNetworkName "myVnet" -SubnetName "mySubnet" -SecurityGroupName   "myNetworkSecurityGroup" -PublicIpAddressName "myPublicIpAddress" -PublicIpSku Standard -OpenPorts 80,3389 -Size Standard_D2_v4 
